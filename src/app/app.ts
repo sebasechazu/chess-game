e69d150a541ff93c1b13779f88d1ae2d1a47ad5e
Sebas Echazú <sebastianechazu@outlook.com>
2025-08-06 10:26:05 -0300

    feat(chess): add chess piece component and modal game component
    - Implemented `ChessPieceComponent` to represent individual chess pieces with properties such as type, color, position, and image.
    - Created `ModalGameComponent` for displaying modals with animations, including backdrop and content transitions.
    - Added SVG images for all chess pieces (black and white) to enhance visual representation in the game.
    - Improved accessibility by handling backdrop clicks and keyboard events for modal closure.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChessGameComponent } from "./chess-game/chess-game.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ ChessGameComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
